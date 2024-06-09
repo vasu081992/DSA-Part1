fcc6fef0101caf72dc0b816e24b5e88acd1a9a1a
vasuve_altair <vasuve@altair.com>
2024-06-09 19:24:08 +0530

    back tracking revison

--- Basic_Recursion/Backtracking_N_to_1.js
+++ Basic_Recursion/Backtracking_N_to_1.js
@@ -1,15 +1,28 @@
 
 function printNumberbackTrack(i,n) {
-
-  if(i>n) return; // base condition to end the recursion 
-
+ 
+  if(i>n){
+    return
+  }
 
   printNumberbackTrack(i+1,n)
 
- console.log(i) // we do recursive function call above and then print here.. this is back tracking. function call happens till condition fails and then it returns till first and prints console line 
+  console.log(i)
 
- //but catch here is console.log comes after function call. and printing happens from reverse to first one by one on returning
-
+ 
 }
 let n=3
 printNumberbackTrack(1,n)
+
+
+
+
+
+// if(i>n) return; // base condition to end the recursion 
+
+
+// printNumberbackTrack(i+1,n)
+
+// console.log(i) // we do recursive function call above and then print here.. this is back tracking. function call happens till condition fails and then it returns till first and prints console line 
+
+// //but catch here is console.log comes after function call. and printing happens from reverse to first one by one on returning
