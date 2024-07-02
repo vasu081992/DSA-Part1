585d6da3de98cdf4ef5d1bde7af546d01c8a2739
vasuve_altair <vasuve@altair.com>
2024-07-02 16:14:20 +0530

    dsa star pattern with spaces

--- Patterns/Pattern7.js
+++ Patterns/Pattern7.js
@@ -1,27 +1,113 @@
 function Pattern7 (N) {
 
   for(i=0;i<N;i++){
- 
-     let res='';
 
-    //space
-    for(j=0;j<N-i-1;j++){
-      res += ' ';
+    let res='';
+
+    //spaces
+    for(j=1;j<=N-i;j++){
+   
+        res=res+' ';
+
+    }
+      //star
+      for(j=0;j<2*i+1;j++){
+   
+        res=res+'*';
+
     }
 
-    // star printing 
-    for(j=0;j<2*i+1;j++){
-      res += '*';
-    }
-    // space printing at end
+    for(j=1;j<=N-i;j++){
+   
+      res=res+' ';
 
-    for(j=0;j<N-i-1;j++){
-      res += ' ';
-    }
+  }
+
+
     console.log(res)
 
   }
- 
+
+
 }
 
 Pattern7(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(i=0;i<N;i++){
+ 
+//   let res='';
+
+//  //space
+//  for(j=0;j<N-i-1;j++){
+//    res += ' ';
+//  }
+
+//  // star printing 
+//  for(j=0;j<2*i+1;j++){
+//    res += '*';
+//  }
+//  // space printing at end
+
+//  for(j=0;j<N-i-1;j++){
+//    res += ' ';
+//  }
+//  console.log(res)
+
+// }
