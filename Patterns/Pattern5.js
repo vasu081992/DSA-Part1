f25784e90606981dd040a79322d7a6d321a1e277
vasuve_altair <vasuve@altair.com>
2024-07-02 15:46:35 +0530

    dsa revisit pattern6

--- Patterns/Pattern5.js
+++ Patterns/Pattern5.js
@@ -1,10 +1,18 @@
 function Pattern5 (N) {
 
-  
+  for(i=1;i<=N;i++){
+    let res='';
+    for(j=i;j<=N;j++){
+     
+      res=res+'*';
+
+    }
+    console.log(res)
+  }
 
 }
 
-Pattern5(7)
+Pattern5(5)
 
 
 
--- Patterns/Pattern6.js
+++ Patterns/Pattern6.js
@@ -1,13 +1,51 @@
 function Pattern6 (N) {
 
-  for(i=1;i<=N;i++){
-    let res=' '
-    for(j=1;j<=N-i+1;j++){
-       res = res+' '+j;
+
+  
+  for(i=0;i<=N;i++){
+    let res='';
+    for(j=1;j<=N-i;j++){
+     
+      res=res+j;
+
     }
-    console.log(res);
+    console.log(res)
+  
   }
-    
+
+
+
 }
 
 Pattern6(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(i=1;i<=N;i++){
+//   let res=' '
+//   for(j=1;j<=N-i+1;j++){
+//      res = res+' '+j;
+//   }
+//   console.log(res);
+// }
+  
