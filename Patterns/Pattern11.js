7e0e3033e2d830130efb16b9df611cee06dbb586
vasuve_altair <vasuve@altair.com>
2024-07-03 11:37:05 +0530

    pattern 1 revision

--- Patterns/Pattern11.js
+++ Patterns/Pattern11.js
@@ -1,24 +1,124 @@
 function Pattern11 (N) {
 
-  for(i=0;i<N;i++){
- 
-     let res='';
-    
-     let start = 1;
-     if(i%2===0){
-     start=1; 
-     }
-     else {
-      start=0;
-     }
-   for(j=0;j<i+1;j++){
-    res = res + start
-    start=1-start;
-   }
-   console.log(res)
-      }
-    } 
-  
-  
 
-Pattern11(5)
+for(i=0;i<N;i++){
+
+  let res='';
+
+//nums printing 
+
+
+for(j=1;j<=i+1;j++){
+
+  res=res+1
+
+}
+
+  //spaces 
+for(j=0;j<N+1-(2*i);j++){
+
+
+  res=res+' '
+
+
+}
+
+//nums printing 
+
+
+for(j=1;j<=i+1;j++){
+
+  res=res+1
+
+}
+
+
+
+
+console.log(res)
+}
+
+
+
+}
+
+Pattern11(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(i=0;i<N;i++){
+ 
+//   let res='';
+ 
+//   let start = 1;
+//   if(i%2===0){
+//   start=1; 
+//   }
+//   else {
+//    start=0;
+//   }
+// for(j=0;j<i+1;j++){
+//  res = res + start
+//  start=1-start;
+// }
+// console.log(res)
+//    }
