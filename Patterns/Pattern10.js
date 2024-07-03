71d900581e575317ba0d69bbef70d4b9968043b6
vasuve_altair <vasuve@altair.com>
2024-07-03 10:41:10 +0530

    pattern10 recoding

--- Patterns/Pattern10.js
+++ Patterns/Pattern10.js
@@ -1,24 +1,88 @@
 function Pattern10 (N) {
 
-  for(i=0;i<(2*N)-1;i++){
+  for(i=1;i<=2*N-1;i++){
  
-     let res='';
+    let res="";
 
-     if (i>=N){
-      for(j=0;j<(2*N)-(i+1);j++){
-        res+='*'
-         }
-     }
-     else {
-     for(j=0;j<i+1;j++){
-     res+='*'
-      }
+    if(i>N){
+
+      res='';
+
+      for(j=1;j<=2*N-i;j++){
+        res=res+'*';
     }
-      console.log(res)
-     }
+  }
+  else{
+    for(j=1;j<=i;j++){
 
+  res= res +"*"
 
-
+    }
   }
 
-Pattern10(3)
+    console.log(res)
+
+  }
+}
+
+
+Pattern10(6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(i=0;i<(2*N)-1;i++){
+ 
+//   let res='';
+
+//   if (i>=N){
+//    for(j=0;j<(2*N)-(i+1);j++){
+//      res+='*'
+//       }
+//   }
+//   else {
+//   for(j=0;j<i+1;j++){
+//   res+='*'
+//    }
+//  }
+//    console.log(res)
+//   }
