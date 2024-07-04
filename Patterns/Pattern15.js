27281595214f739e6bf70127f52fa8a735aafb48
vasuve_altair <vasuve@altair.com>
2024-07-04 09:29:22 +0530

    pattern15

--- Patterns/Pattern15.js
+++ Patterns/Pattern15.js
@@ -1,28 +1,105 @@
 function Pattern15 (N) {
 
-  let num=65;
-
+  
   for(i=1;i<=N;i++){
-    
 
-     let res=' ';
-  
-     // numbers 
-   for(j=1;j<=i;j++){
-  
-     res=res+' '+String.fromCharCode(num);
-  
-  
-   }
+    let charCode = 65;
+    let res='';
 
-   num++;
-  
-   console.log(res)
-  
-      }
-  
+    let char='A';
+
+    for(j=1;j<=N-i+1;j++){
+
+
+     res=res+' '+String.fromCharCode(charCode);
+
+     charCode=charCode+1;
+
+
+    }
+
+console.log(res)
+  }
+
+
+
+
     } 
   
   
     Pattern15(6)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   let num=65;
+
+  // for(i=1;i<=N;i++){
+    
+
+  //    let res=' ';
+  
+  //    // numbers 
+  //  for(j=1;j<=i;j++){
+  
+  //    res=res+' '+String.fromCharCode(num);
+  
+  
+  //  }
+
+  //  num++;
+  
+  //  console.log(res)
+  
+  //     }
   
