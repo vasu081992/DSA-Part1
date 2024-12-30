cd03498ca1c05d07a37213401ca4d5dea9210a91
vasuve_altair <vasuve@altair.com>
2024-12-30 14:08:32 +0530

    DSA arrays - leetcode-1752

--- Arrays/Array-sort-check_rotate-Leetcode1752.js
+++ Arrays/Array-sort-check_rotate-Leetcode1752.js
@@ -15,11 +15,60 @@ function ArraySort_Rotate(arr){
     }
  }
 
-arr[n-1]>arr[0] && count++
-return count<2? true:false
+
+arr[n-1]>arr[0] && count++ //if array is already sorted 
+
+
+return count<2? true:false //js shorthand to return true or false
 
 }
 
 
-let arr=[3,4,5,1,2]
+let arr=[5,6,7,8,1,9]
 console.log(ArraySort_Rotate(arr))
+
+
+
+
+
+//revision
+
+
+
+
+
+
+function ArraySortCheck_Rotate(arr2){
+
+  let count=0;
+
+
+  for(let i=0;i<arr2.length-2;i++){
+    if(arr2[i]>arr2[i+1]){
+      count++
+    }
+  }
+
+  // if(arr[arr.length-1]>arr[0]){
+  //   count++
+  // }
+
+  //below is shorthand of above code
+
+  arr2[arr2.length-1]>arr2[0] && count++
+
+  // if(count>2){
+  //   return false
+  // }
+  // else{
+  //   return true
+  // }
+
+  //below is shorthand of above code
+
+  return count<2? true : false
+}
+
+
+let arr2 = [3,4,5,1,2,9]
+console.log(ArraySortCheck_Rotate(arr2))
