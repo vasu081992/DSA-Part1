c3e719ea821680951153da00b6e3b1d699b9862f
vasuve_altair <vasuve@altair.com>
2025-01-06 15:14:08 +0530

    Array zero-to-end leet code

--- Arrays/Move_Zeros_leetcode-283.js
+++ Arrays/Move_Zeros_leetcode-283.js
@@ -4,3 +4,48 @@
 
 
 
+//optimised two pointer approach - revision - moving zeros to end 
+
+
+
+function MoveZeros_to_End(arr4){
+
+  let n =arr4.length; 
+
+  let j=-5; // some non positive number to keep track 
+
+
+  for(i=0;i<n;i++){
+    if(arr4[i]===0){
+      j=i;
+      break;
+    }
+  }
+
+  if(j===-5){
+    console.log('There is no zero in array')
+    return arr4
+  }
+
+  if(j===0 && arr4.every((val)=> val===0)){
+    console.log('All elems of array are zero')
+    return arr4
+  }
+
+  for(i=j+1;i<n;i++){
+ 
+    if(arr4[i]!==0){
+      [arr4[i],arr4[j]] = [arr4[j],arr4[i]]
+      j++
+    }
+
+  }
+
+  return arr4
+
+}
+
+let arr4=[1,2,3,4,0,8,10,15,0,0,96]
+console.log(MoveZeros_to_End(arr4))
+
+
