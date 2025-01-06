c7171dea823fe11469a8c76399aee8f4101e9f6a
vasuve_altair <vasuve@altair.com>
2025-01-06 14:47:28 +0530

    Array zero-to-end-optimised

--- Arrays/Move_Zeros_to_End.js
+++ Arrays/Move_Zeros_to_End.js
@@ -65,3 +65,48 @@ return arr2
 
 let arr2 = [1,0,5,0,0,9,8,7,3,15]
 console.log(MoveZeros_to_end(arr2))
+
+
+
+
+//jan 6 - optimised approach - move zeros to end  
+
+
+
+function MoveZeros_Optimised(arr3){
+
+  let n = arr3.length;
+
+   let j=-1; 
+
+
+   //placing j in first zero th element 
+
+   for(let i=0;i<n;i++){
+    if(arr3[i]===0){
+       j=i
+       break
+    }
+   }
+
+   //if j is still -1 which means all elems are non-zero in array,
+   //in this case we return the same array
+
+   if(j===-1){
+    return arr3;
+   }
+
+   //after j , we position i as j+1, and if i is not zero we swap, else we go to next i, till i reaches end 
+   //finally we return input array 
+   for(let i=j+1;i<n;i++){
+    if(arr3[i]!==0){  
+      [arr3[i],arr3[j]]=[arr3[j],arr3[i]]
+      j++
+    }
+   }
+return arr3
+}
+
+
+let arr3=[1,0,2,3,0,4,1,0]
+console.log(MoveZeros_Optimised(arr3))
