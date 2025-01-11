a7955f8eba5a4fbf248fc2737b93ca8da628acfd
vasuve_altair <vasuve@altair.com>
2025-01-11 13:40:39 +0530

    array missing num - hashing method

--- Arrays/Missing_num-Array.js
+++ Arrays/Missing_num-Array.js
@@ -50,3 +50,62 @@ console.log('Hash',hash1)
 let arr10 = [1,2,3,5]
 let N=5
 console.log(missNum_Hashing(arr10,N))
+
+//sc and tc are O(N) and O(N)
+
+
+
+//revision of brute force and hashing way 
+
+
+
+
+function Brute_Revisit(arr11,N2){
+
+  for(i=1;i<=N2;i++){
+    let flag=0;
+    for(j=0;j<N-1;j++){
+      if(arr11[j]===i){
+        flag=1
+        break;
+      }
+    }
+    if(flag===0){
+      return i
+    }
+
+  }
+
+  return -1
+
+}
+let N2=5
+let arr11=[1,2,4,5]
+
+console.log(Brute_Revisit(arr11,N2))
+
+
+//revisit of optimised way - hashing 
+
+
+
+function MissingNum_hashing(arr12,N3){
+
+  //creating an empty array 
+
+  let hash2 = new Array(N3+1).fill(0)
+   
+  for(i=0;i<N3-1;i++){
+    hash2[arr12[i]]++;
+  }
+
+  for(i=1;i<=hash2.length;i++){
+    if(hash2[i]===0){
+      return i
+    }
+  }
+
+}
+let arr12=[1,2,4,5]
+let N3=5
+console.log(MissingNum_hashing(arr12,N3))
