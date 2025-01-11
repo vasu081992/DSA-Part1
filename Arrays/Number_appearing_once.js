999a3d1c25bcadd660f3aea4eaf16d31b7824928
vasuve_altair <vasuve@altair.com>
2025-01-11 17:39:07 +0530

    counting the num freq - hashing way

--- Arrays/Number_appearing_once.js
+++ Arrays/Number_appearing_once.js
@@ -1,4 +1,40 @@
-//Brute force approach
+// brute force approach 
+
+function Counting_num(arr2){
+
+  let n = arr2.length;
+
+
+
+  for(i=0;i<n;i++){
+
+    let count = 0; 
+
+    let num = arr2[i];
+
+for(j=0;j<n;j++){
+
+    if(num===arr2[j]){
+
+      count++
+
+    }
+}
+if(count===1){
+  return num;
+}
+}
+return -1
+}
+
+let arr2=[5,5,5,3,3,3,3,3,3,9]
+
+console.log(Counting_num(arr2))
+
+
+
+
+//Using map object
 
 function Number_appearing_once(arr1){
 
@@ -20,3 +56,40 @@ function Number_appearing_once(arr1){
 }
 let arr1=[2,2,3,3,3,5]
 console.log(Number_appearing_once(arr1))
+
+
+//using hashing
+
+
+
+function FrequencyNum_Hashing(arr3){
+
+  let n = arr3.length;
+
+    // Find the maximum element:
+    let maxi = arr3[0];
+    for (let i = 0; i < n; i++) {
+        maxi = Math.max(maxi, arr3[i]);
+    }
+
+    console.log("Maxi",maxi)
+    // Declare hash array of size maxi+1
+    // And hash the given array:
+
+    let hash = new Array(maxi + 1).fill(0);
+    for (let i = 0; i < n; i++) {
+        hash[arr3[i]]++;
+    }
+
+    console.log("Hash",hash)
+    
+    for(let i=0;i<n;i++){
+      if(hash[arr3[i]]===1){
+        return arr3[i]
+      }
+    }
+
+}
+
+let arr3=[8,8,8,5,3,3,9,9]
+console.log(FrequencyNum_Hashing(arr3))
