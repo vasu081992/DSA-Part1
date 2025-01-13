5f7bfca356a21b58363fe6bbc6658c17108664db
vasuve_altair <vasuve@altair.com>
2025-01-13 15:35:07 +0530

    single number - hashing-leetcode136

---
+++ Arrays/Leetcode_136_Singlenumber.js
@@ -0,0 +1,42 @@
+function Num_appearing_once_hashing(arr3){
+ 
+  let n=arr3.length;
+
+  let max = -Infinity; // Initialize max to the smallest possible value
+  let min = Infinity;  // Initialize min to the largest possible value
+
+  //finding max elem of array used for creating a hash array
+ if(n===1){
+  return arr3[0]
+ }
+ else{
+  for(let item of arr3){
+    if(item>max){
+      max=item
+    }
+    if (item < min) {
+      min = item;
+    }
+  }
+
+  const offset = Math.abs(min);
+
+  let hash1= new Array(max+offset+1).fill(0)
+
+
+  
+  for(let i=0;i<n;i++){
+    hash1[arr3[i]+offset]++
+  }
+
+
+  for(let i=0;i<arr3.length;i++){
+    if(hash1[arr3[i]+offset]===1){
+      return arr3[i]
+    }
+  }
+ }
+  
+}
+let arr3 = [11,11,11,12,13,13]
+console.log(Num_appearing_once_hashing(arr3))
--- Arrays/Numbering_appearing_once-revisit.js
+++ Arrays/Numbering_appearing_once-revisit.js
@@ -75,15 +75,21 @@ function Num_appearing_once_hashing(arr3){
  
   let n=arr3.length;
 
-  let max = 0;
+  let max = 0; // Initialize max to the smallest possible value
+
 
   //finding max elem of array used for creating a hash array
-
+ if(n===1){
+  return arr3[0]
+ }
+ else{
   for(let item of arr3){
     if(item>max){
       max=item
     }
+
   }
+
 
   let hash1= new Array(max+1).fill(0)
 
@@ -99,7 +105,7 @@ function Num_appearing_once_hashing(arr3){
       return arr3[i]
     }
   }
-
+ }
   
 }
 let arr3 = [11,11,11,12,13,13]
