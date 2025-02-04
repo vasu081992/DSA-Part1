c00efde713514f0f2c700a17cc1c9b2d98d4c399
vasuve_altair <vasuve@altair.com>
2025-02-04 15:22:33 +0530

    num appearing once - using hashing - feb4

--- Arrays/Number_appearing_once-feb1.js
+++ Arrays/Number_appearing_once-feb1.js
@@ -77,3 +77,74 @@ function Number_Visiting_Once_2(arr10){
 
 let arr10=[5,5,6,6,10,7,7,7,7,7,9,9,9]
 console.log(Number_Visiting_Once_2(arr10))
+
+
+
+//optimisied -1 - feb4 
+
+
+function Num_appearing_once(arr11){
+
+  let map1 = new Map()
+
+
+  for(let num in arr11){
+    map1.set(arr11[num], (map1.get(arr11[num]) || 0 ) + 1);
+  }
+  
+  for(let [key,value] of map1){
+    if(value===1){
+      return key
+    }
+  }
+
+}
+
+let arr11 = [7,9,9,10,10,14,14,5,7]
+console.log(Num_appearing_once(arr11))
+
+
+
+
+//optimisied 2 - using Hashing 
+
+
+
+function Num_appearing_once_hashing(arr12){
+
+  let n = arr12.length;
+   let max=0;
+
+  for(let i=0; i < arr12.length ; i++){
+
+    if(arr12[i]>max){
+
+      max=arr12[i];
+
+    }
+
+  }
+
+  let newArr = new Array(max+1).fill(0)
+  
+
+  for(let i=0;i<n;i++){
+ 
+    newArr[arr12[i]]++;
+
+  }
+
+
+  for(let i=0;i<n;i++){
+
+    if(newArr[i]===1){
+       return i
+
+  }
+
+}
+}
+
+
+let arr12 = [7,7,6,8,6,4,7,6,8,8,10,10]
+console.log(Num_appearing_once_hashing(arr12))
