365e8bb9cacd16b68d1134a9fbdc0b925bc81691
vasuve_altair <vasuve@altair.com>
2025-02-12 13:18:20 +0530

    leet code 560 commit

--- Arrays/Total_subarrays-leetcode-560.js
+++ Arrays/Total_subarrays-leetcode-560.js
@@ -11,12 +11,12 @@ function Num_of_Subarrays(nums,k){
 
   let prefixSum = new Map()
 
-  let count= 0; 
+  let count = 0; 
 
-  let sum =0 ;
+  let sum = 0 ;
 
 
-  prefixSum.set(0,1)
+  prefixSum.set(0,1) // we use this to increment count whenver 0 is encountered
 
   for(let item of nums){
  
@@ -30,7 +30,7 @@ function Num_of_Subarrays(nums,k){
     prefixSum.set(sum, (prefixSum.get(sum) || 0 + 1))
   }
   return count
-  
+
 }
 
 let nums = [3, 4, 7, 2, -3, 1, 4, 2] 
@@ -38,3 +38,35 @@ let nums = [3, 4, 7, 2, -3, 1, 4, 2]
 let k = 7
 
 console.log(Num_of_Subarrays(nums,k))
+
+
+//revisit of same problem 
+
+
+
+function Subarray_count(arr2,k2){
+
+  let map1 = new Map() 
+
+  let count = 0 
+
+  let sum = 0 
+ 
+  map1.set(0,1) 
+
+  for(let num of arr2){
+    sum = sum + num; 
+    
+     if(map1.has(sum-k2)){
+      count = count + map1.get(sum-k2)
+     }
+   
+     map1.set(sum, (map1.get(sum) || 0 + 1 ))
+  }
+  return count
+}
+
+let arr2 = [2,3,5,2,2,1]
+let k2 = 5
+
+console.log(Subarray_count(arr2,k2))
